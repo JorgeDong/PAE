99ac567825eef5ca6a66d923a90a8c6ede12f48f
Ricardo Díaz <39528804+ricardodf@users.noreply.github.com>
2020-05-10 11:44:41 -0500

    Login and Registration UI + Services
    - Edit and change UI for login and registration that had some big issues
    - New folder 'services'

---
+++ pae/src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  getUsers(){
+
+  }
+
+  getUserById(){
+
+  }
+
+  getUsersByEmail(){
+
+  }
+
+  editUserById(){
+
+  }
+
+  editUserByEmail(){
+
+  }
+
+  
+
+}
