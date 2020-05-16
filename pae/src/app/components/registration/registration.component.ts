7b1f1493f18bdbbc4e9bc5b29351a0ee6a7a2633
Ricardo Díaz <39528804+ricardodf@users.noreply.github.com>
2020-05-16 02:20:26 -0500

    Minor correction in buttons

--- pae/src/app/components/registration/registration.component.ts
+++ pae/src/app/components/registration/registration.component.ts
@@ -12,7 +12,7 @@ import { Credito } from 'src/app/models/Credito';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  inputName = '';
+  inputName: string;
   inputEmail = '';
   inputPassword1 = '';
   inputPassword2 = '';
@@ -26,37 +26,79 @@ export class RegistrationComponent implements OnInit {
 
   inputCredito = 0;
 
+  alertEmptyValues = true;
+  alertPassMismatch = true;
+  alertPassLength = true;
+  alertEmailExists = true;
+  alertInvalidCredit = true;
+
   constructor(private userService: UserService, private router: Router, private authService: AuthService) { }
 
-  ngOnInit(): void {
-  }
-  submitReg(form: NgForm){
-    this.userService.registration(this.inputName,
-                                  this.inputEmail,
-                                  this.inputPassword1,
-                                  this.inputPassword2,
-                                  this.inputDireccion,
-                                  this.inputCity,
-                                  this.inputCountry,
-                                  this.inputPregunta1,
-                                  this.inputRespuesta1,
-                                  this.inputPregunta2,
-                                  this.inputRespuesta2).subscribe(
-      (data: User) => {
-            console.log('Currentdata: ' + data);
-            this.userService.createUserCredito(data.id, this.inputCredito).subscribe(
-              (credito: Credito) => {
-                console.log('Credit done: ' + credito);
-                this.router.navigate(['/login']);
-              }
-            );
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+  ngOnInit(): void {}
+
+  submitReg(form: NgForm) {
+    if (form.valid) {
+            if (this.inputPassword1 !== this.inputPassword2) {
+              this.alertPassMismatch = false;
+              return;
+            }
+            if (this.inputPassword1.length < 6 ||  this.inputPassword2.length < 6) {
+              this.alertPassLength = false;
+              return;
+            }
+            if (+this.inputCredito > 0) {
+              this.alertInvalidCredit = false;
+            }
+            this.alertEmptyValues = true;
+            this.userService.registration(this.inputName,
+            this.inputEmail,
+            this.inputPassword1,
+            this.inputPassword2,
+            this.inputDireccion,
+            this.inputCity,
+            this.inputCountry,
+            this.inputPregunta1,
+            this.inputRespuesta1,
+            this.inputPregunta2,
+            this.inputRespuesta2).subscribe(
+        (data: User) => {
+          console.log('Current data: ' + data);
+          this.userService.createUserCredito(data.id, this.inputCredito).subscribe(
+            (credito: Credito) => {
+              console.log('Credit done: ' + credito);
+              this.router.navigate(['/login']);
+            }
+          );
+        },
+        (err) => this.alertEmailExists = false
+      );
+    } else {
+      this.alertEmptyValues = false;
+    }
   }
 
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
 
-  
+  closeEmptyValues() {
+    this.alertEmptyValues = true;
+  }
+  closePassMismatch() {
+    this.alertPassMismatch = true;
+  }
+  closePassLength() {
+    this.alertPassLength = true;
+  }
+  closeEmailExists() {
+    this.alertEmailExists = true;
+  }
+  closeInvalidCredit() {
+    this.alertInvalidCredit = true;
+  }
+
 }
